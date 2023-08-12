3e856372494679bd2a06b0f483604d0d59270ae7
dibyendu-mahata <dibyendumahata2016@gmail.com>
2023-08-12 12:22:21 +0530

    Changes

--- app/src/main/java/com/androidactivity/foregroundservices_android/MainActivity.kt
+++ app/src/main/java/com/androidactivity/foregroundservices_android/MainActivity.kt
@@ -3,13 +3,24 @@ package com.androidactivity.foregroundservices_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.androidactivity.foregroundservices_android.ui.theme.ForegroundServicesAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +28,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ForegroundServicesAndroidTheme {
-                Text(text = "One")
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Foreground Services",
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.size(120.dp))
+                        Text(text = "(Please, add the Notification Permission.)")
+                        Spacer(modifier = Modifier.size(20.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "End Service")
+                            }
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "Start Service")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
